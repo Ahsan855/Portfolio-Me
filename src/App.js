2773975f9d68fcb1f39a73ad5fdb4bb05bdcfc7b
Ahsan855e <a.r.ahsanhabib08@gmail.com>
2022-06-09 13:44:42 +0600

    done setup my portfolio

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
+import Navbar from "./Pages/Navbar/Navbar";
+import Service from "./Pages/Service/Service";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
